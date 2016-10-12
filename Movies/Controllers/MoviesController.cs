0a5f8aad3199493b9287f3bad5ed1e03bb8dc6c7
edgar7386 <edgarreyesrojas@gmail.com>
2016-10-11 22:32:02 -0500

    init

---
+++ Movies/Controllers/MoviesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Movies.Controllers
+{
+    public class MoviesController : ApiController
+    {
+
+        [HttpGet]
+        public IHttpActionResult GetMovies()
+        {
+            moviesContext db = new moviesContext();
+            List<Movies> items = db.Movies.ToList();
+            return Ok();
+        }
+        [HttpGet]
+        public IHttpActionResult GetMovie(Guid id)
+        {
+            moviesContext db = new moviesContext();
+            Movies items = db.Movies.Where(x => x.ID == id).FirstOrDefault();
+            db.Dispose();
+            return Ok(items);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetSaludo()
+        {
+           // List<int> l = new List<int>() { 1, 2, 3 };
+            return Ok("Holi");
+            // return Ok(new List<int>() { 1, 2, 3 });
+        }
+        [HttpPost]
+        public IHttpActionResult SetMovie(Movies movie)
+        {
+            moviesContext db = new moviesContext();
+            db.Movies.Add(movie);
+            db.SaveChanges();
+            return Ok("OK");
+
+        }
+
+    }
+}
---
+++ Movies/Scripts/appMovies.js
@@ -0,0 +1,23 @@
+﻿var app = angular.module('appMovies',[])
+var Controllers = {};
+    Controllers.newMovie = function ($scope,$http) {
+    $scope.save = function () {
+        console.log($scope.Movie);
+        $scope.result = JSON.stringify($scope.Movie);
+        //$http.get("/api/movies/GetSaludo").then(
+        //    function (data) {
+        //        console.log(data);
+        //    },
+        //    function (data) {
+        //        console.log(data);
+        //    });
+        $http.post("/api/movies/SetMovie",$scope.Movie).then(
+            function (data) {
+                console.log(data);
+            },
+            function (data) {
+                console.log(data);
+            });
+    }
+}
+app.controller(Controllers);
